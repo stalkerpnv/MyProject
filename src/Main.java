e7b9b17184d0cdab87b4f22cafa1d98dac18fc51
stalkerpnv <stalker_pnv@mail.ru>
2024-04-19 14:14:21 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
